652f9d2971f73b2a711ff40918b76bde14acb460
Lungu Andrei <54952551+0ndrei@users.noreply.github.com>
2021-04-29 18:41:42 +0300

    Ver 0.6

---
+++ DigitaLife/js/menu.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+/* Buton go-to-shop */
+function goshop() {
+  location.replace("Products.html");
+}
