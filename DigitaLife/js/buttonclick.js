1d56df56ce5cedf33df44c5444518b2df306b53b
Lungu Andrei <54952551+0ndrei@users.noreply.github.com>
2021-05-10 17:54:11 +0300

    Ver 1.3

---
+++ DigitaLife/js/buttonclick.js
@@ -0,0 +1,13 @@
+/* Buton credit */
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+/* Buton go-to-shop */
+function goshop() {
+    location.replace("Products.html");
+}
